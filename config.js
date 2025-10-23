2282780cfa6af472db34231b2e117a2e7335bb10
Diego Alejandro Medina Martinez <101026833+otpvayne@users.noreply.github.com>
2025-10-23 00:35:49 -0500

    Change API_BASE string quotes from single to double

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Cambia esta URL por la de tu servicio en Render (por ejemplo: https://mi-api.onrender.com)
-const API_BASE = 'https://guia-2-backend.onrender.com';
+const API_BASE = "https://guia-2-backend.onrender.com";
